b0bc1f105018d1c85092a2082192ec14eb51e674
shivamsingha <shivamgit@pm.me>
2024-02-26 14:02:00 +0000

    Start adding stuff

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module example/hello
 go 1.22.0
 
 require (
-	github.com/a-h/templ v0.2.543 // indirect
+	github.com/a-h/templ v0.2.543
+	github.com/gofiber/fiber/v2 v2.52.1
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/gofiber/fiber/v2 v2.52.1 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
+
+	"example/hello/view"
 )
 
 func main() {
@@ -15,17 +17,19 @@ func main() {
 	app.Get("/:name?", func(c *fiber.Ctx) error {
 		name := c.Params("name")
 		if name == "" {
-			name = "World"
-		}
-		return Render(c, Home(name))
+			name = "World"}
+		return Render(
+			c, view.Home(name))
 	})
 	app.Use(NotFoundMiddleware)
 
-	log.Fatal(app.Listen(":3000"))
+	log.Fatal(
+		app.Listen(":3000"))
 }
 
 func NotFoundMiddleware(c *fiber.Ctx) error {
-	return Render(c, NotFound(), templ.WithStatus(http.StatusNotFound))
+	return Render(
+		c, view.NotFound(), templ.WithStatus(http.StatusNotFound))
 }
 
 func Render(c *fiber.Ctx, component templ.Component, options ...func(*templ.ComponentHandler)) error {
