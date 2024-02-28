8d090de4597021782d320177a662750e9a611531
shivamsingha <shivamgit@pm.me>
2024-02-28 17:25:05 +0000

    Add 404 page, helmet, logger

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/a-h/templ"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
+	"github.com/gofiber/fiber/v2/middleware/helmet"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 
 	"example/hello/view"
 	"example/hello/view/layout"
@@ -13,17 +17,31 @@ import (
 
 func main() {
 	app := fiber.New()
+	app.Use(helmet.New())
+	app.Use(logger.New())
 	app.Static("/public", "./public")
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return Render(c, layout.Base(view.Index()))
 	})
+
 	app.Get("/login", func(c *fiber.Ctx) error {
 		return Render(c, layout.Base(partial.Login()))
 	})
 
+
+	app.Get("/signup", func(c *fiber.Ctx) error {
+		return Render(c, layout.Base(partial.Signup()))
+	})
+
+	app.Use(NotFoundMiddleware)
+
 	log.Fatal(
 		app.Listen(":3000"))
+}
+
+func NotFoundMiddleware(c *fiber.Ctx) error {
+	return Render(c, layout.Base(partial.Error404()), templ.WithStatus(http.StatusNotFound))
 }
 
 func Render(c *fiber.Ctx, component templ.Component, options ...func(*templ.ComponentHandler)) error {
