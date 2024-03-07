d79ea8945cfb92e896ee56a51b217528105ca73f
shivamsingha <shivamgit@pm.me>
2024-03-07 15:07:01 +0000

    Add pgxpool

--- cmd/webapp/main.go
+++ cmd/webapp/main.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 	"github.com/gofiber/fiber/v2/middleware/helmet"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/jackc/pgx/v5"
+	"github.com/gofiber/fiber/v2/middleware/session"
+	"github.com/gofiber/storage/postgres/v2"
+	"github.com/jackc/pgx/v5/pgxpool"
 
 	"example/hello/internal/middleware"
 	database "example/hello/internal/model"
@@ -21,13 +24,33 @@ import (
 )
 
 func main() {
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	dbConfig, err := pgxpool.ParseConfig(os.Getenv("DATABASE_URL"))
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), dbConfig)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
-	defer conn.Close(context.Background())
+
+	conn, err := pool.Acquire(context.Background())
+	if err != nil {
+		log.Fatal("Error while acquiring connection from the database pool!!")
+	}
+
+	defer conn.Release()
+
 	q := database.New(conn)
+
+	storage := postgres.New(postgres.Config{
+		DB:         pool,
+		Table:      "fiber_storage",
+		Reset:      false,
+		GCInterval: 10 * time.Second,
+	})
+
+	store := session.New(session.Config{
+		Storage: storage,
+	})
 
 	app := fiber.New()
 	app.Use(helmet.New())
@@ -36,19 +59,24 @@ func main() {
 
 	app.Get("/", util.Render(layout.Base(view.Index())))
 
-	app.Route("/login", func(login fiber.Router) {
-		login.Get("/", util.Render(layout.Base(partial.Login())))
-		login.Post("/", service.LoginHandler(q))
+	app.Route("/login", func(router fiber.Router) {
+		router.Get("/", util.Render(layout.Base(partial.Login())))
+		router.Post("/", service.LoginHandler(q))
 	})
 
-	app.Route("/signup", func(signup fiber.Router) {
-		signup.Get("/", util.Render(layout.Base(partial.Signup())))
-		signup.Post("/", service.SignupHandler(q))
+	app.Route("/signup", func(router fiber.Router) {
+		router.Get("/", util.Render(layout.Base(partial.Signup())))
+		router.Post("/", service.SignupHandler(q))
 	})
 
-	app.Route("/forgot-password", func(forgot fiber.Router) {
-		forgot.Get("/", util.Render(layout.Base(partial.ForgotPassword())))
-		forgot.Post("/", service.ForgotPassword(q))
+	app.Route("/forgot-password", func(router fiber.Router) {
+		router.Get("/", util.Render(layout.Base(partial.ForgotPassword())))
+		router.Post("/", service.ForgotPassword(q))
+	})
+
+	app.Route("/reset-password", func(router fiber.Router) {
+		router.Get("/", util.Render(layout.Base(partial.ResetPassword())))
+		// router.Post("/")
 	})
 
 	app.Use(middleware.NotFoundMiddleware())
--- go.mod
+++ go.mod
@@ -16,9 +16,11 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/gofiber/storage/postgres/v2 v2.0.3 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -30,6 +32,7 @@ require (
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
